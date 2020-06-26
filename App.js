c18c07329d4e0de19bfd446e650376ab5e7ee1a2
Melvin Hernandez <melher39@gmail.com>
2020-06-25 22:07:22 -0500

    pretty much deleted the entire app.js and now working with the styling

--- App.js
+++ App.js
@@ -1,152 +1,36 @@
-import React, { useState } from 'react';
+import React from "react";
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Button,
-} from 'react-native';
+  View
+} from "react-native";
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// import {
+//   Colors
+// } from "react-native/Libraries/NewAppScreen";
 
-// const App = () => {
-//   return (
-//     <>
-//       <StatusBar barStyle="dark-content" />
-//       <SafeAreaView>
-//         <ScrollView
-//           contentInsetAdjustmentBehavior="automatic"
-//           style={styles.scrollView}>
-//           <Header />
-//           {global.HermesInternal == null ? null : (
-//             <View style={styles.engine}>
-//               <Text style={styles.footer}>Engine: Hermes</Text>
-//             </View>
-//           )}
-//           <View style={styles.body}>
-//             <View style={styles.sectionContainer}>
-//               <Text>Hello world</Text>
-//               <Text style={styles.sectionTitle}>Step One</Text>
-//               <Text style={styles.sectionDescription}>
-//                 Edit <Text style={styles.highlight}>App.js</Text> to change this
-//                 screen and then come back to see your edits.
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>See Your Changes</Text>
-//               <Text style={styles.sectionDescription}>
-//                 <ReloadInstructions />
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Debug</Text>
-//               <Text style={styles.sectionDescription}>
-//                 <DebugInstructions />
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Learn More</Text>
-//               <Text style={styles.sectionDescription}>
-//                 Read the docs to discover what to do next:
-//               </Text>
-//             </View>
-//             <LearnMoreLinks />
-//           </View>
-//         </ScrollView>
-//       </SafeAreaView>
-//     </>
-//   );
-// };
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const handlePress = () => {
-    setCounter(counter + 1);
-  };
-  const [greeting, setGreeting] = useState("What's good son");
-
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          <View style={styles.body}>
-            <Greeting greeting="greeting1" />
-            <Greeting greeting={greeting} />
-            {/* <Text
-              style={styles.sectionTitle}>
-              {counter}
-            </Text> */}
-            <Button
-              onPress={() => setGreeting("Let's get it!")}
-              title="Change greeting"
-            />
-            {/* <Button
-              onPress={handlePress}
-              title="Touch Me"
-            />
-            <Button
-              onPress={() => setCounter(0)}
-              title="Reset"
-              color="red"
-            /> */}
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={styles.container}>
+      <View style={styles.box}></View>
+      <View style={styles.box}></View>
+      <View style={styles.box}></View>
+    </View>
   );
 };
 
-const Greeting = ({ greeting }) => <Text>{greeting}</Text>
-
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter
+  container: {
+    flex: 1,
+    // flexDirection: "row"
+    justifyContent: "space-between",
+    alignItems: "center"
   },
-  engine: {
-    position: "absolute",
-    right: 0
-  },
-  body: {
-    backgroundColor: Colors.white
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600",
-    color: Colors.black,
-    textAlign: "center"
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-    color: Colors.dark
-  },
-  highlight: {
-    fontWeight: "700"
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: "600",
-    padding: 4,
-    paddingRight: 12,
-    textAlign: "right"
+  box: {
+    height: 100,
+    width: 100,
+    backgroundColor: "red",
+    margin: 10
   }
 });
 
