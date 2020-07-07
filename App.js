20602587ed3a9f361d93c5e647eed37e85372d94
Melvin Hernandez <melher39@gmail.com>
2020-07-06 22:17:15 -0500

    imported FlatList and building on top of the doc example. leaving off here

--- App.js
+++ App.js
@@ -3,25 +3,34 @@ import {
   StyleSheet,
   View,
   TouchableHighlight,
-  Text
+  Text,
+  FlatList
 } from "react-native";
 
 const App = () => {
-  const renderItems = () => {
-    return people.map(renderItem);
-  };
-  const renderItem = (item, index) => {
-    return <Text key={index} style={styles.text}>{item.name} is {item.age} years old</Text>
-  };
+  const renderItem = ({ item }) =>
+    <View style={{ padding: 15, borderBottomColor: "#ddd", borderBottomWidth: 1 }}>
+      <Text style={{ fontSize: 20 }}>{item.name}</Text>
+      <Text style={{ color: "rgba(0,0,0,.5)" }}>Age: {item.age}</Text>
+    </View>
+    ;
+
   const people = [
     { name: "Melvin", age: 29 },
     { name: "Jessica", age: 25 },
     { name: "Jocelyn", age: 17 },
+    { name: "Jason", age: 20 },
+    { name: "Martina", age: 21 },
+    { name: "Abel", age: 32 },
   ];
 
   return (
     <View style={styles.container}>
-      {renderItems()}
+      <FlatList
+        data={people}
+        keyExtractor={(item, index) => item.name}
+        renderItem={renderItem}
+      />
     </View>
   );
 };
