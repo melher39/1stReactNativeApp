10d3437b436239100653afe2cd0c0a947523eb1e
Melvin Hernandez <melher39@gmail.com>
2020-07-14 20:55:02 -0500

    added a pull down to refresh function and it works! it populates the screen with more people's names and ages. accomplished this with hooks

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   View,
@@ -7,28 +7,46 @@ import {
   FlatList
 } from "react-native";
 
+const people = [
+  { name: "Melvin", age: 29 },
+  { name: "Jessica", age: 25 },
+  { name: "Jocelyn", age: 17 },
+  { name: "Jason", age: 20 },
+  { name: "Martina", age: 21 },
+  { name: "Jackie", age: 37 }
+];
+
 const App = () => {
+  const [names, setNames] = useState(people);
+  const [refreshing, setRefreshing] = useState(false);
+  const onRefresh = () => {
+    console.log("refreshing...")
+    setRefreshing(true);
+    setTimeout(() => {
+      const newPeople = [
+        ...names,
+        { name: "Tony", age: 32 },
+        { name: "John", age: 40 },
+        { name: "Amanda", age: 25 }
+      ];
+      setNames(newPeople);
+      setRefreshing(false);
+    }, 2400);
+    console.log("new names: ", names);
+  };
   const renderItem = ({ item }) =>
     <View style={{ padding: 15, borderBottomColor: "#ddd", borderBottomWidth: 1 }}>
       <Text style={{ fontSize: 20 }}>{item.name}</Text>
       <Text style={{ color: "rgba(0,0,0,.5)" }}>Age: {item.age}</Text>
-    </View>
-    ;
-
-  const people = [
-    { name: "Melvin", age: 29 },
-    { name: "Jessica", age: 25 },
-    { name: "Jocelyn", age: 17 },
-    { name: "Jason", age: 20 },
-    { name: "Martina", age: 21 },
-    { name: "Abel", age: 32 },
-  ];
+    </View>;
 
   return (
     <View style={styles.container}>
       <FlatList
-        data={people}
-        keyExtractor={(item, index) => item.name}
+        onRefresh={onRefresh}
+        refreshing={refreshing}
+        data={names}
+        keyExtractor={(item, index) => item.name + index}
         renderItem={renderItem}
       />
     </View>
