c51d127fa24b1b47a99f5e276f535ffffeb6f7c3
Melvin Hernandez <melher39@gmail.com>
2020-07-21 20:02:42 -0500

    day-3 initial commit. created a very basic username and password input form

--- App.js
+++ App.js
@@ -3,85 +3,49 @@ import {
   StyleSheet,
   View,
   Text,
-  FlatList
+  TextInput,
+  TouchableHighlight,
+  Button
 } from "react-native";
 
-// const people = [
-//   { name: "Melvin", age: 29 },
-//   { name: "Jessica", age: 25 },
-//   { name: "Jocelyn", age: 17 },
-//   { name: "Jason", age: 20 },
-//   { name: "Martina", age: 21 },
-//   { name: "Jackie", age: 37 }
-// ];
-
 const App = () => {
-  const [names, setNames] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await fetch("https://swapi.dev/api/people/");
-        const json = await data.json();
-        setNames(json.results);
-      }
-      catch (err) {
-        console.log(err);
-      }
-    };
-    fetchData();
-  }, []);
-  const [refreshing, setRefreshing] = useState(false);
-  // const onRefresh = () => {
-  //   console.log("refreshing...")
-  //   setRefreshing(true);
-  //   setTimeout(() => {
-  //     const newPeople = [
-  //       ...names,
-  //       { name: "Tony", age: 32 },
-  //       { name: "John", age: 40 },
-  //       { name: "Amanda", age: 25 }
-  //     ];
-  //     setNames(newPeople);
-  //     setRefreshing(false);
-  //   }, 2400);
-  //   console.log("new names: ", names);
-  // };
-  const onRefresh = async () => {
-    console.log("refreshing...");
-    setRefreshing(true);
-    try {
-      const data = await fetch("https://swapi.dev/api/people/");
-      const json = await data.json();
-      if (json.next) {
-        const moreData = await fetch(json.next);
-        const json2 = await moreData.json();
-        setNames([...names, ...json2.results]);
-      } else {
-        setNames(json.results);
-      }
-      setRefreshing(false);
-    }
-    catch (err) {
-      console.log(err);
-    }
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
+  const [input, setInput] = useState({ username: "", password: "", formData: "" });
+  const onChangeText = (key, val) => {
+    setInput(input => ({ ...input, [key]: val }));
   };
 
-  const renderItem = ({ item }) =>
-    <View style={{ padding: 15, borderBottomColor: "#ddd", borderBottomWidth: 1 }}>
-      <Text style={{ fontSize: 20 }}>{item.name}</Text>
-      <Text style={{ color: "rgba(0,0,0,.5)" }}>Gender: {item.gender}</Text>
-    </View>;
+  const submit = () => {
+    const userData = {
+      username: input.username,
+      password: input.password,
+      signedIn: true
+    };
+    setInput(input => ({ ...input, formData: JSON.stringify(userData) }));
+  };
 
   return (
     <View style={styles.container}>
-      <Text>People:</Text>
-      <FlatList
-        onRefresh={onRefresh}
-        refreshing={refreshing}
-        data={names}
-        keyExtractor={(item, index) => item.name + index}
-        renderItem={renderItem}
+      <Text>Form Demo</Text>
+      <TextInput
+        placeholder="Username"
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={val => onChangeText("username", val)}
+        style={styles.input}
       />
+      <TextInput
+        placeholder="Password"
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={val => onChangeText("password", val)}
+        style={styles.input}
+        secureTextEntry
+      />
+      <Button onPress={submit} title="Submit" />
+      <Text>Input State: {input.username} and {input.password} and {input.formData}</Text>
+      <Text>state formData: {input.formData}</Text>
     </View>
   );
 };
@@ -89,12 +53,14 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     paddingVertical: 30,
     paddingHorizontal: 14
   },
-  text: {
-    fontSize: 22
+  input: {
+    padding: 8,
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#666"
   }
 });
 
